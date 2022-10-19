7ea926312c6c4aa76c43c9f488217be1b2c139b5
isaac.lu <isaac.lu0897@gmail.com>
2022-10-19 18:09:36 +0800

    add automount.sh for auto mount installation image

---
+++ tools/automount.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mount.cifs //192.168.87.9/Users/Administrator/Downloads/os_images /var/www/ds/images -o username=administrator,uid=$(id -u),gid=$(id -g)
+
+# ubuntu
+
+# 22.04.1
+mount -o loop,x-mount.mkdir /var/www/ds/images/ubuntu-22.04.1-desktop-amd64.iso /var/www/ds/images_file/ubuntu/22_04_1
+
+# redhat
+
+# 8.5.0
+mount -o loop,x-mount.mkdir /var/www/ds/images/RHEL-8.5.0-20211013.2-x86_64-dvd1.iso /var/www/ds/images_file/redhat/8_5_0
+
+# 8.6.0
+mount -o loop,x-mount.mkdir /var/www/ds/images/RHEL-8.6.0-20220420.3-x86_64-dvd1.iso /var/www/ds/images_file/redhat/8_6_0
+
+# 9.1.0
+mount -o loop,x-mount.mkdir /var/www/ds/images/RHEL-9.1.0-20220829.0-x86_64-dvd1.iso /var/www/ds/images_file/redhat/9_1_0
+
+
+
