1e79c5cbfe449e23ab0999d828b2438188721925
isaac.lu <isaac.lu0897@gmail.com>
2024-03-11 07:25:44 +0800

    Update automake.sh

--- tools/automake.sh
+++ tools/automake.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 ipxe_source_path="ipxe/src"
-script_path="../../tools/script.ipxe"	# The path of script_path is relative to ipxe_source_path
 
 echo make clean all
 make -C $ipxe_source_path clean
+rm -rf $ipxe_source_path/bin*
 
-echo start to make undionly.kpxe
-make -C $ipxe_source_path -j 16 bin-x86_64-pcbios/undionly.kpxe EMBED=$script_path &> /dev/null && echo make undionly.kpxe successed || echo "failed to make undionly.kpxe"
+
+echo start to make ipxe.pxe undionly.kpxe
+make -C $ipxe_source_path -j 16 bin-x86_64-pcbios/{ipxe.pxe,undionly.kpxe} &> /dev/null && echo make pcbios successed || echo "failed to make pcbios"
+echo copy file
+cp $ipxe_source_path/bin-x86_64-pcbios/ipxe.pxe ipxe.pxe
+cp $ipxe_source_path/bin-x86_64-pcbios/undionly.kpxe undionly.kpxe
 
 echo start to make ipxe.efi and snponly.efi
-make -C $ipxe_source_path -j 16 bin-x86_64-efi/{ipxe.efi,snponly.efi} EMBED=$script_path &> /dev/null && echo make ipxe.efi and snponly.efi successed || echo "failed to make ipxe.efi and snponly.efi"
-
-echo link file
-
-ln -snf $ipxe_source_path/bin-x86_64-pcbios/undionly.kpxe undionly.kpxe
-ln -snf $ipxe_source_path/bin-x86_64-efi/ipxe.efi ipxe.efi
-ln -snf $ipxe_source_path/bin-x86_64-efi/snponly.efi snponly.efi
+make -C $ipxe_source_path -j 16 bin-x86_64-efi/{ipxe.efi,snponly.efi} &> /dev/null && echo make efi successed || echo "failed to make efi"
+echo copy file
+cp $ipxe_source_path/bin-x86_64-efi/ipxe.efi ipxe.efi
+cp $ipxe_source_path/bin-x86_64-efi/snponly.efi snponly.efi
 
 echo done
